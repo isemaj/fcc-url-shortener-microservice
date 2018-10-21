0e8ddf2f522666b7138f707eae8043ab7f48a1be
James Itum <jamesitum@protonmail.ch>
2018-10-21 15:04:15 +0800

    create POST functionality

---
+++ controllers/urlHandler.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const UrlEntries = require("../models/urlEntries");
+const dns = require("dns");
+const shortid = require("shortid");
+
+const protocolRegExp = /^https?:\/\/(.*)/i;
+const hostnameRegExp = /^([a-z0-9\-_]+\.)+[a-z0-9\-_]+/i;
+const forwardSlashRegexp = /\/$/i;
+
+exports.addUrl = (req, res) => {
+  let url = req.body.url;
+
+  if (url.match(forwardSlashRegexp)) {
+    url = url.slice(0, -1);
+  }
+
+  let protocolMatch = url.match(protocolRegExp);
+  if (!protocolMatch) {
+    return res.json({"error": "invalid URL"});
+  }
+
+  let hostAndQuery = protocolMatch[1];
+  let hostnameMatch = hostAndQuery.match(hostnameRegExp);
+
+  if (hostnameMatch) {
+    dns.lookup(hostnameMatch[0], (err) => {
+      if (err) {
+        res.json({"error": "invalid Hostname"});
+      } else {
+        let convertedLink = Buffer.from(hostnameMatch[0]).toString("base64");
+        UrlEntries.findOne({ "url": convertedLink }, (err, data) => {
+          if (err) return;
+          if (data) {
+            res.json({"original_url": url, "short_url": data.short});
+          } else {
+            let shortLink = shortid.generate();
+            const newLink = new UrlEntries({
+              "url": convertedLink, 
+              "short": shortLink 
+            });
+            newLink.save((err, data) => {
+              if (err) return; 
+              res.json({"original_url":  url, "short_url": shortLink});
+            })
+          }
+        })
+      }
+    });
+  } else {
+    res.json({"error": "invalid URL" });
+  }
+}
+
+exports.checkUrl = (req, res) => {
+  const hashurl = req.params.hashurl;
+  UrlEntries.findOne({ "short": hashurl }, (err, data) => {
+    if (err) return;
+    if (data) {
+      const trueUrl = Buffer.from(data.url, "base64").toString("utf8");
+      console.log(trueUrl)
+      console.log('in here')
+      res.redirect(trueUrl);
+    } else {
+      console.log("another here")
+      res.json({"error": "No shorturl found"});
+    }
+  })
+}
+
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const mongo = require("mongodb");
+
+const urlHandler = require("./controllers/urlHandler.js");
+
 const MongoClient = mongo.MongoClient;
 
 const PORT = process.env.PORT || 9000;
+
+const connectOption= {
+  useNewUrlParser: true,
+};
 
 const app = express();
 
@@ -13,14 +21,20 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // check the dbpath in /etc/mongodb.conf
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI, connectOption);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({"extended": false}));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
+
+app.post("/api/shorturl/new", urlHandler.addUrl);
+app.get("/api/shorturl/:hashurl", urlHandler.checkUrl)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
