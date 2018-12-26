25049c617ad327c022e0b3b9805a68f8385b7423
James Itum <39540851+isemaj@users.noreply.github.com>
2018-12-26 15:01:45 +0800

    remove log for DB url

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 // check the dbpath in /etc/mongodb.conf
 const mongoURI = process.env.NODE_ENV !== "production" ? `${process.env.MONGODB_URI_DEV}${process.env.DB}` : process.env.MONGODB_URI;
-console.log(mongoURI);
 mongoose.connect(mongoURI, connectOption)
   .then(() => console.log("MONGODB connected..."))
   .catch(err => console.log(err));
